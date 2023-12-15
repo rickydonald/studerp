d80776eaa5c11bcd1bbe6bf0808584465e307db6
rickydonald <rickydonaldr@gmail.com>
2023-12-15 23:59:26 +0530

    Header Button removed from Attendance Card

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -104,11 +104,6 @@ export default function DashboardScreen() {
         <View className="bg-white mx-3 my-6 rounded-xl">
           <View className="p-3 flex-row items-center justify-between border-b border-b-slate-300">
             <Text className="font-bold" style={{ fontSize: 22 }}>Today's Attendance</Text>
-            <TouchableOpacity
-              className=""
-            >
-              <Text style={{ color: appleSystemBlue }} className="text-center font-bold">Show in Detail</Text>
-            </TouchableOpacity>
           </View>
           <View className="p-3">
             <View className="flex-row items-center justify-between">
@@ -128,12 +123,12 @@ export default function DashboardScreen() {
                 <Text style={attendanceMicroBoxStyle.text}>V</Text>
               </View>
             </View>
-            {/* <TouchableOpacity
+            <TouchableOpacity
               className="mt-4"
               style={{ backgroundColor: appleSystemGrayLight5, padding: 10, borderRadius: 10 }}
             >
               <Text className="text-center font-semibold">Show Attendance</Text>
-            </TouchableOpacity> */}
+            </TouchableOpacity>
           </View>
         </View>
 
