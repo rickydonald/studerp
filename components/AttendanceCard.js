49bb2bbbdde4975ecff83b12a6b95644545a2298
rickydonald <rickydonaldr@gmail.com>
2023-12-20 23:47:48 +0530

    Attendance card changed

--- components/AttendanceCard.js
+++ components/AttendanceCard.js
@@ -3,34 +3,36 @@ import React from 'react'
 
 import { appleSystemGreen, appleSystemRed, appleSystemBlue } from '../src/Config'
 
-export default function AttendanceCard() {
+export default function AttendanceCard({
+    date = "Nil",
+    dayOrder = "Nil",
+    hour1,
+    hour2,
+    hour3,
+    hour4,
+    hour5
+}) {
     return (
-        <View className="bg-white mx-3 my-6 rounded-xl">
-            <View className="p-3 flex-row items-center justify-between border-b border-b-slate-300">
-                <Text className="font-bold" style={{ fontSize: 22 }}>Today's Attendance</Text>
-                <TouchableOpacity
-                    className=""
-                >
-                    <Text style={{ color: appleSystemBlue }} className="text-center font-bold">Show in Detail</Text>
-                </TouchableOpacity>
+        <View className="bg-white px-5 flex-row items-center justify-between">
+            <View>
+                <Text className="uppercase font-bold" style={{ fontSize: 14 }}>{ date }</Text>
+                <Text className="uppercase font-bold" style={{ fontSize: 14 }}>{ dayOrder }</Text>
             </View>
-            <View className="p-3">
-                <View className="flex-row items-center justify-between">
-                    <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                        <Text style={attendanceMicroBoxStyle.text}>I</Text>
-                    </View>
-                    <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                        <Text style={attendanceMicroBoxStyle.text}>II</Text>
-                    </View>
-                    <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
-                        <Text style={attendanceMicroBoxStyle.text}>III</Text>
-                    </View>
-                    <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
-                        <Text style={attendanceMicroBoxStyle.text}>IV</Text>
-                    </View>
-                    <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                        <Text style={attendanceMicroBoxStyle.text}>V</Text>
-                    </View>
+            <View className="flex-row items-center justify-between gap-2">
+                <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
+                    <Text style={attendanceMicroBoxStyle.text}>I</Text>
+                </View>
+                <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
+                    <Text style={attendanceMicroBoxStyle.text}>II</Text>
+                </View>
+                <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
+                    <Text style={attendanceMicroBoxStyle.text}>III</Text>
+                </View>
+                <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
+                    <Text style={attendanceMicroBoxStyle.text}>IV</Text>
+                </View>
+                <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
+                    <Text style={attendanceMicroBoxStyle.text}>V</Text>
                 </View>
             </View>
         </View>
@@ -39,17 +41,18 @@ export default function AttendanceCard() {
 
 const attendanceMicroBoxStyle = StyleSheet.create({
     container: {
-      padding: 10,
-      borderRadius: 10,
-      width: 50,
-      height: 50,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
+        padding: 10,
+        borderRadius: 10,
+        width: 40,
+        height: 40,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     text: {
-      color: '#000',
-      fontSize: 18,
-      fontWeight: 'bold',
+        display: "none",
+        color: '#000',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
-  })
+})
--- screens/AttendanceScreen.js
+++ screens/AttendanceScreen.js
@@ -15,7 +15,7 @@ export default function AttendanceScreen() {
   return (
     <SafeAreaView className="bg-white flex-1" edges={['top']}>
       <Header screenName={"Attendance"} />
-      <ScrollView className="flex-1" style={{ backgroundColor: appleSystemGrayLight6 }}>
+      <ScrollView className="flex-1">
         <View className="bg-white px-5 pb-5 flex-1">
           <View className="mt-5 flex-row items-center justify-between">
             <View>
@@ -47,10 +47,9 @@ export default function AttendanceScreen() {
         <View>
           <Calendar
             style={{
-              height: 350
+              height: 350,
             }}
-            // Specify the current date
-            current={'2012-03-01'}
+            className="mb-5"
             // Callback that gets called when the user selects a day
             onDayPress={day => {
               console.log('selected day', day);
@@ -62,54 +61,16 @@ export default function AttendanceScreen() {
               '2023-12-03': { selected: true, selectedColor: appleSystemBlue }
             }}
           />
-          <View className="bg-white px-5 pb-5 flex-row items-center justify-between">
-            <View>
-              <Text className="uppercase font-bold" style={{ fontSize: 14 }}>14 Dec 23</Text>
-              <View style={{ borderBottomColor: appleSystemGray, borderBottomWidth: StyleSheet.hairlineWidth, marginVertical: .5 }}></View>
-              <Text className="uppercase font-bold" style={{ fontSize: 14 }}>A3</Text>
-            </View>
-            <View className="flex-row items-center justify-between gap-2">
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>I</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>II</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>III</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemRed }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>IV</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>V</Text>
-              </View>
-            </View>
-          </View>
-          <View className="bg-white px-5 pb-5 flex-row items-center justify-between">
-            <View>
-              <Text className="uppercase font-bold" style={{ fontSize: 14 }}>15 Dec 23</Text>
-              <View style={{ borderBottomColor: appleSystemGray, borderBottomWidth: StyleSheet.hairlineWidth, marginVertical: .5 }}></View>
-              <Text className="uppercase font-bold" style={{ fontSize: 14 }}>b3</Text>
-            </View>
-            <View className="flex-row items-center justify-between gap-2">
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>I</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>II</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>III</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>IV</Text>
-              </View>
-              <View style={[{ backgroundColor: appleSystemGreen }, attendanceMicroBoxStyle.container]}>
-                <Text style={attendanceMicroBoxStyle.text}>V</Text>
-              </View>
-            </View>
-          </View>
+          {/* Attendance Cards */}
+          <AttendanceCard
+            date='15 Dec 23'
+            dayOrder='b3'
+          />
+          <View style={{ borderBottomColor: appleSystemGray, borderBottomWidth: StyleSheet.hairlineWidth, marginTop: 15, marginBottom: 15 }} className="px-5"></View>
+          <AttendanceCard
+            date='16 Dec 23'
+            dayOrder='c3'
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
