f40b38d435a8ba1d80dfdccdb78033c00ab2367b
rickydonald <rickydonaldr@gmail.com>
2023-12-21 14:39:05 +0530

    App.js plugin updates

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import * as IconsOutline from 'react-native-heroicons/outline';
 import * as IconsSolid from 'react-native-heroicons/solid';
@@ -70,20 +72,24 @@ export default function App() {
   return (
     <>
       <StatusBar style='auto' />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            gestureEnabled: true,
-            gestureDirection: "horizontal",
-            ...TransitionPresets.SlideFromRightIOS,
-          }}
-          headerMore="float"
-        >
-          <Stack.Screen name="Home" component={LoginStack} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+                gestureEnabled: true,
+                gestureDirection: "horizontal",
+                ...TransitionPresets.SlideFromRightIOS,
+              }}
+              headerMore="float"
+            >
+              <Stack.Screen name="Home" component={LoginStack} />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </>
   );
 }
--- screens/AttendanceScreen.js
+++ screens/AttendanceScreen.js
@@ -1,17 +1,46 @@
 import { View, Text, Button, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useMemo, useRef, useCallback } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native';
 import Header from '../components/Header';
 import AttendanceCard from '../components/AttendanceCard';
 
-import { appleSystemBlue, appleSystemFillGray10, appleSystemGrayLight6, appleSystemGreen, appleSystemRed, appleSystemGrayLight5, appleSystemGray } from '../src/Config';
+import { appleSystemBlue, appleSystemFillGray10, appleSystemGrayLight6, appleSystemGreen, appleSystemRed, appleSystemGrayLight5, appleSystemGray, appleSystemOrange, appleSystemGray3 } from '../src/Config';
 import { Calendar } from 'react-native-calendars';
 import { InformationCircleIcon } from 'react-native-heroicons/outline';
+
+import { useBottomSheetDynamicSnapPoints, BottomSheetView, BottomSheetBackdrop, BottomSheetModal } from '@gorhom/bottom-sheet';
+import HorizontalLine from '../components/HorizontalLine';
 
 export default function AttendanceScreen() {
 
   const navigation = useNavigation();
+
+  const bottomSheetRef = useRef(null)
+  const initialSnapPoints = useMemo(() => ['CONTENT_HEIGHT'], []);
+  const {
+    animatedHandleHeight,
+    animatedSnapPoints,
+    animatedContentHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
+
+  const handleOpenPress = (postId, instanceId, userId = null) => {
+    bottomSheetRef.current?.present()
+  }
+
+  const handleClosePress = () => { bottomSheetRef.current.close(); setSelectedPostId(null); }
+  const renderBackdrop = useCallback(
+    props => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={-1}
+        appearsOnIndex={0}
+        pressBehavior={'close'}
+      />
+    ),
+    []
+  );
 
   return (
     <SafeAreaView className="bg-white flex-1" edges={['top']}>
@@ -45,6 +74,7 @@ export default function AttendanceScreen() {
             </TouchableOpacity>
             <TouchableOpacity
               style={{ backgroundColor: appleSystemGrayLight5, padding: 10, borderRadius: 10 }}
+              onPress={() => handleOpenPress()}
             >
               <InformationCircleIcon color={"#000"} />
             </TouchableOpacity>
@@ -90,6 +120,49 @@ export default function AttendanceScreen() {
           />
         </View>
       </ScrollView>
+      <BottomSheetModal
+        backdropComponent={renderBackdrop}
+        ref={bottomSheetRef}
+        index={0}
+        snapPoints={animatedSnapPoints}
+        handleHeight={animatedHandleHeight}
+        contentHeight={animatedContentHeight}
+        backgroundStyle={{ backgroundColor: 'rgb(242, 242, 247)', borderRadius: 24 }}
+        enablePanDownToClose={true}
+      >
+        <BottomSheetView
+          style={{ flex: 1, padding: 24 }}
+          onLayout={handleContentLayout}
+        >
+          <View className="pb-5 px-2 pt-0" style={{ marginBottom: 25 }}>
+            <Text className="font-bold mb-6" style={{ fontSize: 22 }}>Color Markings</Text>
+            <View className="flex-row items-center justify-between">
+              <View style={{ backgroundColor: appleSystemGreen, width: 40, height: 40, borderRadius: 10 }}></View>
+              <Text className="font-semibold" style={{ fontSize: 18 }}>Present</Text>
+            </View>
+            <HorizontalLine />
+            <View className="flex-row items-center justify-between">
+              <View style={{ backgroundColor: appleSystemRed, width: 40, height: 40, borderRadius: 10 }}></View>
+              <Text className="font-semibold" style={{ fontSize: 18 }}>Absent</Text>
+            </View>
+            <HorizontalLine />
+            <View className="flex-row items-center justify-between">
+              <View style={{ backgroundColor: appleSystemBlue, width: 40, height: 40, borderRadius: 10 }}></View>
+              <Text className="font-semibold" style={{ fontSize: 18 }}>On Duty (OD)</Text>
+            </View>
+            <HorizontalLine />
+            <View className="flex-row items-center justify-between">
+              <View style={{ backgroundColor: appleSystemOrange, width: 40, height: 40, borderRadius: 10 }}></View>
+              <Text className="font-semibold" style={{ fontSize: 18 }}>Medical Leave (ML)</Text>
+            </View>
+            <HorizontalLine />
+            <View className="flex-row items-center justify-between">
+              <View style={{ backgroundColor: appleSystemGray3, width: 40, height: 40, borderRadius: 10 }}></View>
+              <Text className="font-semibold" style={{ fontSize: 18 }}>Not Marked</Text>
+            </View>
+          </View>
+        </BottomSheetView>
+      </BottomSheetModal>
     </SafeAreaView>
   )
 }
